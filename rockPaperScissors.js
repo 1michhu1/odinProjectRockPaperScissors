d6089207157e7bab201f0836ec7910235debd363
1michhu1 <michh2005@gmail.com>
2023-07-31 00:19:52 -0400

    add rock paper scissors ui (display score and results)

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,64 +1,79 @@
-function getComputerChoice(choices) {
+let numRoundsPlayed = 0
+let compChoices = ['rock', 'paper', 'scissors']
+let playerScore = 0
+let compScore = 0
+const resultRef = document.querySelector("#roundResultText")
+const totalScoreRef = document.querySelector("#totalScoreResultText")
+
+function getCompChoice(choices) {
     return choices[ Math.floor(Math.random() * choices.length)]
 };
 
-function playRound( playerSelection, computerSelection ) {
+function getGameResult( playerSelection, computerSelection ) {
+    // return game result as a number: 0=draw, 1=player wins, 2=computer wins
+    // and also returns game resultStr
     playerSelection = playerSelection.toLowerCase()
-    resultStr = "adsf"
+    resultStr = ""
+    resultNum = 0
+
     playerWin = ( (playerSelection == "paper" && computerSelection == 'rock')
     || (playerSelection == "scissors" && computerSelection == 'paper') 
     || (playerSelection == "rock" && computerSelection == "scissors")
     )
-
+    
     if ( playerSelection == computerSelection ) {
         resultStr = "Draw!"
     
     
     } else if (playerWin) {
         resultStr = `You win! ${playerSelection} beats ${computerSelection}!`
+        resultNum = 1
     } else {
         resultStr = `You lose! ${computerSelection} beats ${playerSelection}!`
+        resultNum = 2
 
     }
 
-    return resultStr
+    return [resultNum, resultStr]
 };
 
-function getPlayerChoice() {
-    console.log(this.getAttribute('id'))
+function increaseScore(roundResult) {
+    if (roundResult === 1) {
+        playerScore += 1
+    }
+    else if (roundResult === 2) {
+        compScore += 1
+    }
+}
+
+function playRound() {
+    if (numRoundsPlayed < 5) {
+
+        playerSelection = this.getAttribute('id')
+        compSelection = getCompChoice(compChoices)
+
+        resultArr = getGameResult(playerSelection, compSelection)
+        resultNum = resultArr[0]
+        resultStr = resultArr[1]
+        resultRef.textContent = resultStr
+
+        increaseScore(resultNum)
+        totalScoreStr = `Player: ${playerScore} Computer: ${compScore}`
+        totalScoreRef.textContent = totalScoreStr
+        
+        if (resultNum) {
+            numRoundsPlayed += 1
+        }
+        
+    }
 
 }
 
 function game() {
-    // pressing buttons calls play round function
-    // change playerSelection value to choice based on which button pressed
-    compChoices = ['rock', 'paper', 'scissors']
     const btns = document.querySelectorAll("button")
-    btns.forEach( btn => btn.addEventListener(
-        "click", 
-        () => {
-                playerSelection = btn.getAttribute("id")
-                compSelection = getComputerChoice(compChoices)
-                console.log(`player: ${playerSelection}, comp: ${compSelection}`)
-                result = playRound(playerSelection, compSelection)
-                console.log(result)
-            }
-        )
-    )
+    btns.forEach( btn => btn.addEventListener("click", playRound))
     
-    // compChoices = ['rock', 'paper', 'scissors'];
-    
-    // playerSelection = prompt("enter choice: rock, paper or scissors", "rock");
-    // computerSelection = getComputerChoice( compChoices )
-    // result = playRound( playerSelection, computerSelection );
-    // console.log(`playersleection: ${playerSelection}`)
-    // console.log(`computerseleection: ${computerSelection}`)
-
-
-    // console.log(result);
-
-    };
+};
 
 game()
 
-// console.log(getComputerChoice(['rock', 'paper', 'scissors']));
